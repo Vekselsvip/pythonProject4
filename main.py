ebeda4a951677482be2b66ca6216b677c075ab5d
vekselsvip <veksel44@gmail.com>
2022-09-24 18:24:03 +0300

    ok

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from telebot import types
 import telebot
 import os
 
+
 app = Flask(__name__)
-TOKEN = os.environ.get('TOKEN')
+TOKEN = '5416512263:AAFvQ7A8kR1AyqQN15IO4BWkOgqYaEzLBXA'
 bot = telebot.TeleBot(TOKEN)
 
 my_dict = {'AT-AT': 1.40, 'AT-BE': 1.40, 'AT-CH': 1.75, 'AT-CZ': 1.40, 'AT-DE': 1.40, 'AT-DK': 1.75, 'AT-ES': 1.46,
